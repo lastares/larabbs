d6a55b7e2b4d0f2ebc47bb18343d020ec3e970d3
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-07-27 11:36:01 +0800

    [feat:]发布话题

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,16 @@ $api->version('v1', [
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')
                 ->name('api.user.show');
+            // 编辑登录用户信息
+            $api->patch('user', 'UsersController@update')
+                ->name('api.user.update');
+            // 图片资源
+            $api->post('images', 'ImagesController@store')
+                ->name('api.images.store');
+
+            // 发布话题
+            $api->post('topics', 'TopicsController@store')
+                ->name('api.topics.store');
         });
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
@@ -62,12 +72,6 @@ $api->version('v1', [
             ->name('api.authorizations.destroy');
 
 
-        // 编辑登录用户信息
-        $api->patch('user', 'UsersController@update')
-            ->name('api.user.update');
-        // 图片资源
-        $api->post('images', 'ImagesController@store')
-            ->name('api.images.store');
     });
 
 });
